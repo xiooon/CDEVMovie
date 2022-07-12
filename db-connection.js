d0f5dc552d7c7d65c289ccb3070c18da10d1d156
xiooon <99613028+xiooon@users.noreply.github.com>
2022-07-12 15:07:09 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'database-1.cwwgwxgzzts7.us-east-1.rds.amazonaws.com',
     port: '3306',
-    user: 'root',
-    password: 'root',
+    user: 'admin',
+    password: 'password',
     database: 'movie_info'
 });
 
 connection.connect(err => {  // test out connetion and console.log error if there is one
     if (err) throw err;
-    console.log('Connected To DB');
+    console.log('Connected To AWS DB');
 }); 
 module.exports = connection;
 
